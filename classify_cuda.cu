db3d974e656bd57291164f37c4abb8eae1aa39da
lx9t01 <alex.xuhaixiang@gmail.com>
2016-05-05 01:24:38 -0700

    temp

--- classify_cuda.cu
+++ classify_cuda.cu
@@ -59,14 +59,14 @@ void trainLogRegKernel(
                 __syncthreads();
             }
             // printf("%f\n", gradient[0]);
-            // weight_temp[i] = gradient[0]; // the sum is stored in the 0th element
+            weight_temp[i] = gradient[0]; // the sum is stored in the 0th element
         }
 
         if (threadIdx.x == 0) {
             for (int i = 0; i < REVIEW_DIM; ++i) {
                 // the addition of two shared memory result in a batch
-                // atomicAdd(&weights[i], -step_size * weight_temp[i]);
-                atomicAdd(&weights[i], -step_size * gradient[0]);
+                atomicAdd(&weights[i], -step_size * weight_temp[i]);
+                // atomicAdd(&weights[i], -step_size * gradient[0]);
                 // weights[i] = 0;
                 // weights[i] = weights[i] - step_size * weight_temp[i];
             }
