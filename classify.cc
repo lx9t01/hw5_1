0ad1206d478fe82f64d2426aa43d5d70a98cdb4d
lx9t01 <alex.xuhaixiang@gmail.com>
2016-05-04 23:10:32 -0700

    3072

--- classify.cc
+++ classify.cc
@@ -187,7 +187,7 @@ int main(int argc, char** argv) {
     // Init timing
     float time_initial, time_final;
     
-    int batch_size = 16384;
+    int batch_size = 3072;
     
     // begin timer
     time_initial = clock();
